7ba42b99c452888e82c3eb4380607645e6e47eac
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-09-30 14:19:27 +0700

    mengganti nama class dari class manusia menjadi class customer agar study kasus lebih relevan

--- membuat-class/index.php
+++ membuat-class/index.php
@@ -1,12 +1,13 @@
 <!-- Mendefinisikan Class / Membuat Class -->
 <?php 
-class Manusia{
+class Customers{
 
 }
 
 // Membuat beberapa instance dari sebuah object yang mana dalam object ini adalah instance dari class manusia
-$object1 = new Manusia();
-$object2 = new Manusia();
+$object1 = new Customers();
+$object2 = new Customers();
 
+// Menambilkan beberapa object yang sudah dibuat sebelumnya
 var_dump($object1);
 var_dump($object2);
