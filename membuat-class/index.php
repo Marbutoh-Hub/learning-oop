86ceaafb427f491f42a88e6d2f6e7c2e627da31f
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-09-30 13:51:15 +0700

    membuat class dan melakukan instance object dari sebuah class yang telah dibuat

---
+++ membuat-class/index.php
@@ -0,0 +1,12 @@
+<!-- Mendefinisikan Class / Membuat Class -->
+<?php 
+class Manusia{
+
+}
+
+// Membuat beberapa instance dari sebuah object yang mana dalam object ini adalah instance dari class manusia
+$object1 = new Manusia();
+$object2 = new Manusia();
+
+var_dump($object1);
+var_dump($object2);
